5a0bae5137ee8e046e880cdc592565c262ecb5c5
jdicola-clear <dicolajuan@gmail.com>
2022-05-22 18:51:25 -0300

    Inicio del proyecto

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const useSelectMonedas = () => {
+  const SelectMonedas = () => {
+      console.log('desde select monedas');
+  }
+
+  return [SelectMonedas];
+}
+
+export default useSelectMonedas;
